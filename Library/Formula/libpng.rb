d132a10b61e9763c5eca87bb29f9ebeb0111060d
Mojca Miklavec <mojca.miklavec@gmail.com>
2011-01-29 22:09:44 +0100

    remove keg_only flag in libpng (we want to link against it)

--- Library/Formula/libpng.rb
+++ Library/Formula/libpng.rb
@@ -5,7 +5,7 @@ class Libpng <Formula
   homepage 'http://www.libpng.org/pub/png/libpng.html'
   md5 'e3ac7879d62ad166a6f0c7441390d12b'
 
-  keg_only :provided_by_osx
+  # keg_only :provided_by_osx
 
   def install
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
